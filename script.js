9d6ec8d84082a74d87ac1a3f875bc703778faddb
mokhseem <mokhseem@icloud.com>
2022-09-29 13:08:23 +0300

    Add dark theme w/ switcher

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function search() {
-  let query = document.getElementById("search").value;
-  query = "https://www.youtube.com/results?search_query=" + query;
+let page = document.querySelector('.page');
+let theme = document.querySelector('.theme-btn');
+
+theme.onclick = function () {
+  page.classList.toggle('dark-theme');
+};
+
+function search () {
+  let query = document.getElementById('search').value;
+  query = 'https://www.youtube.com/results?search_query=' + query;
   window.location.href = query;
 };
 
