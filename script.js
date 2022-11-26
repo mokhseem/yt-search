205cefaba6c272b7af2816274818c6a95e94b877
mokhseem <mokhseem@icloud.com>
2022-11-26 21:31:16 +0300

    Refactor script

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-function search () {
-  let query = document.getElementById("search").value;
-  query = "https://www.youtube.com/results?search_query=" + query;
-  window.location.href = query;
-};
+const URL = "https://www.youtube.com/results?search_query=";
+const searchButton = document.querySelector('button');
+const searchQuery = document.querySelector('#search');
 
-document.addEventListener('keydown', function (evt) {
+function search() {
+  window.open(URL + searchQuery.value);
+  searchQuery.value = '';
+}
+
+searchButton.addEventListener('click', search);
+searchQuery.addEventListener('keydown', (evt) => {
   if (evt.keyCode === 13) {
     search();
   }
