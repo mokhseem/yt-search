b5b1ecf123450060979091be46ded8241478465b
mokhseem <mokhseem@icloud.com>
2022-11-27 00:58:50 +0300

    Rewrite with arrow function

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const URL = "https://www.youtube.com/results?search_query=";
+const URL = 'https://www.youtube.com/results?search_query=';
 const searchButton = document.querySelector('button');
 const searchQuery = document.querySelector('#search');
 
-function search() {
+const searchVideo = () => {
   window.open(URL + searchQuery.value);
   searchQuery.value = '';
-}
+};
 
-searchButton.addEventListener('click', search);
+searchButton.addEventListener('click', searchVideo);
 searchQuery.addEventListener('keydown', (evt) => {
-  if (evt.keyCode === 13) {
-    search();
+  if (evt.key === 'Enter') {
+    searchVideo();
   }
 });
