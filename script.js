24b7e0a8674606e926ee847974bf9f6ec4b37bb2
mokhseem <mokhseem@outlook.com>
2023-09-15 02:34:28 +0300

    Refactor page source

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 const URL = 'https://www.youtube.com/results?search_query=';
-const searchQuery = document.querySelector('#search');
-const themeButton = document.querySelector('.theme-button');
-const searchButton = document.querySelector('.search-button');
+const searchQuery = document.querySelector('.search__input');
+const searchButton = document.querySelector('.search__button');
 
 const searchVideo = () => {
   window.open(URL + searchQuery.value);
   searchQuery.value = '';
 };
-
-themeButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark-theme');
-});
 
 searchButton.addEventListener('click', searchVideo);
 searchQuery.addEventListener('keydown', (evt) => {
